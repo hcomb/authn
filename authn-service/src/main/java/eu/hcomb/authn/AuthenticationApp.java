a3f99e491872c926580715e7c36f5f2ea1ef2b22
Alessandro Siletto <alessandro.siletto@gmail.com>
2015-12-07 20:54:51 +0100

    datasource health check

--- authn-service/src/main/java/eu/hcomb/authn/AuthenticationApp.java
+++ authn-service/src/main/java/eu/hcomb/authn/AuthenticationApp.java
@@ -1,19 +1,28 @@
 package eu.hcomb.authn;
 
+import java.security.Key;
+
+import javax.crypto.spec.SecretKeySpec;
+
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.Authorizer;
 import io.dropwizard.setup.Environment;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.Base64Codec;
 
 import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.name.Named;
 
 import eu.hcomb.authn.resources.UsernamePasswordLogin;
-import eu.hcomb.authn.resources.WhoAmI;
 import eu.hcomb.authn.service.UserService;
 import eu.hcomb.authn.service.impl.UserServiceImpl;
 import eu.hcomb.common.auth.TokenAuthenticator;
 import eu.hcomb.common.auth.UserAuthorizer;
+import eu.hcomb.common.healthcheck.DatasourceHealthCheck;
+import eu.hcomb.common.resources.WhoAmI;
 import eu.hcomb.common.service.TokenService;
 import eu.hcomb.common.service.impl.TokenServiceImpl;
 import eu.hcomb.common.web.BaseApp;
@@ -44,20 +53,29 @@ public class AuthenticationApp extends BaseApp<AuthenticationConfig> {
 
 	}	
 
+	@Provides
+	@Named("healthcheck.query")
+	public String getHealthCheckQuery(){
+		return ((AuthenticationConfig)configuration).getDatabase().getHealthCheckQuery();
+	}
+
 	@Override
 	public void run(AuthenticationConfig configuration, Environment environment) {
 		
 		defaultConfig(environment, configuration);
 
-		Module persistence = new AuthenticationPersistence(configuration);
+		AuthenticationPersistence persistence = new AuthenticationPersistence(configuration);
 
 		injector = Guice.createInjector(this, persistence);
 
-        setupSecurity(environment, "token", "jwt", "realm", "Bearer");
+        setupSecurity(environment);
         
 		environment.jersey().register(injector.getInstance(UsernamePasswordLogin.class));
 		environment.jersey().register(injector.getInstance(WhoAmI.class));
+		
+		environment.healthChecks().register("mysql", injector.getInstance(DatasourceHealthCheck.class));
 				
 	}
+	
 
 }
